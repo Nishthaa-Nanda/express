3d0dd9faa693a5763f6baddf1411cdebe3ba89c7
Nishthaa-Nanda <nishthananda100@gmail.com>
2025-07-30 23:53:43 +0530

    setting ejs

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+
+app.set("view engine","ejs")
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+  //dynamic route
 // app.get('/profile/:username', (req, res) => {
 //   res.send(`hello from ${req.params.username}`);
 // })
 // app.get(/.*fly$/, (req, res) => {
 //   res.send('/.*fly$/')
 // })
-app.get('/example/b', (req, res, next) => {
-  console.log('the response will be sent by the next function ...')
-  next()
-}, (req, res) => {
-  res.send('Hello from B!')
-})
-
-
-
+// app.get('/example/b', (req, res, next) => {
+//   console.log('the response will be sent by the next function ...')
+//   next()
+// }, (req, res) => {
+//   res.send('Hello from B!')
+// })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
